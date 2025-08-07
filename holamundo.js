3f480bc391ca5f56d3a9ee7b694c3777a1bd63d2
Mauricio Rivero <rivero.mauricio@gmail.com>
2025-08-06 20:41:30 -0500

    cambie el valor del numero1

--- holamundo.js
+++ holamundo.js
@@ -1,5 +1,5 @@
 console.log('Bienvenidos a mi primer programa')
-let numero1 = 5;
+let numero1 = 3;
 let numero2 = 7;
 let resultado = numero1 + numero2;
 console.log('El resultado de la suma es: '+resultado);
